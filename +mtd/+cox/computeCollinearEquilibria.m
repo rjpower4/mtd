a1a9ab8b60227ea72fae18d210797639912d69cc
Rolfe Power <rolfepower4@gmail.com>
2021-12-28 02:35:50 -0500

    Formatting update
    Signed-off-by: Rolfe Power <rolfepower4@gmail.com>

--- +mtd/+cox/computeCollinearEquilibria.m
+++ +mtd/+cox/computeCollinearEquilibria.m
@@ -1,60 +1,76 @@
-function eqPoints = computeCollinearEquilibria(massRatio, ax, options)
-    %COMPUTECOLLINEAREQUILIBRIA calculate the collinear equilibrium solutions in Cox's 
-    %problem
-    %
-    %   The collinear equilibra are those equilibrium solutions that exist on the x-axis
-    %   as defined in the standard CRTBP rotating frame. In Cox's model, these only exist
-    %   when the low-thrust acceleration is oriented parallel (or anti-parallel) to the
-    %   x-axis. Consequently, the `ax` parameter passed to this function is a scalar with
-    %   positive numbers corrensponding to dot(a, x) > 0 and negative corresponding to the
-    %   opposite, i.e., dot(a, x) < 0 where a is the full 3 vector for the low-thrust
-    %   acceleration.
-    arguments
-        massRatio {mtd.crtbp.mustBeAValidMassRatio} % Mass ratio of the system
-        ax (1, 1) double % Acceleration in the x direction
-        options.tol double {mtd.util.mustBeValidTolerance} = 1e-12 % Newton-Raphson function tolerance
-        options.maxiter {mustBeInteger, mustBeGreaterThan(options.maxiter, 1)} = 15 
-    end
+function [eqPoints, didConverge] = computeCollinearEquilibria(massRatio, ax, options)
+%COMPUTECOLLINEAREQUILIBRIA calculate the collinear equilibrium solutions in Cox's
+%problem
+%
+%   The collinear equilibra are those equilibrium solutions that exist on the x-axis
+%   as defined in the standard CRTBP rotating frame. In Cox's model, these only exist
+%   when the low-thrust acceleration is oriented parallel (or anti-parallel) to the
+%   x-axis. Consequently, the `ax` parameter passed to this function is a scalar with
+%   positive numbers corrensponding to dot(a, x) > 0 and negative corresponding to the
+%   opposite, i.e., dot(a, x) < 0 where a is the full 3 vector for the low-thrust
+%   acceleration.
+%
+%   [EQPOINTS] = COMPUTECOLLINEAREQUILIBRIA(MASSRATIO, AX) compute the collinear
+%   equilibrium points for the system with CRTBP mass ratio MASSRATIO and low-thrust
+%   acceleration AX. The returned array is a 3x6 matrix with each row representing a
+%   collinear equilibrium state.
+%
+%   [EQPOINTS, DID_CONVERGE] = COMPUTECOLLINEAREQUILIBRIA(...) compute as above but also
+%   return a matrix a Nx3 logical matrix identifying which equilibrium point solutions did
+%   converge.
+arguments
+    massRatio {mtd.crtbp.mustBeAValidMassRatio}
+    ax (1, 1) double
+    options.tol double {mtd.util.mustBeValidTolerance} = 1e-12
+    options.maxiter {mustBeInteger, mustBeGreaterThan(options.maxiter, 1)} = 15
+end
 
-    eqPoints = zeros(3, 6, length(ax));
+eqPoints = zeros(3, 6, length(ax));
+didConverge = zeros(length(ax), 3);
 
-    for k = 1:length(ax)
-        % Fast path for CRTBP (a = 0)
-        if ax(k) == 0
-            lagrangePoints = crtbpLagrangePoints(massRatio);
-            eqPoints(:, :, k) = lagrangePoints(1:3, :);
-        else
-            eqPoints(:, :, k) = internalSolver(massRatio, ax(k), options);
-        end
+for k = 1:length(ax)
+    % Fast path for CRTBP (a = 0) as these are just the lagrange points
+    if ax(k) == 0
+        lagrangePoints = crtbpLagrangePoints(massRatio);
+        eqPoints(:, :, k) = lagrangePoints(1:3, :);
+        didConverge(k, :) = [true, true, true];
+    else
+        [eqPoints(:, :, k), didConverge(k, :)] = internalSolver(massRatio, ax(k), options);
     end
+end
 
 end
 
-function q = internalSolver(mu, ax, options)
-    % Determine gamma values
-    [guess1, guess2, guess3] = generateInitialGuesses(mu);
-    g1 = simpleNewtonRaphson(@(x) fdf1(mu, ax, x), guess1, options);
-    g2 = simpleNewtonRaphson(@(x) fdf2(mu, ax, x), guess2, options);
-    g3 = simpleNewtonRaphson(@(x) fdf3(mu, ax, x), guess3, options);
+function [q, didConverge] = internalSolver(mu, ax, options)
+% Determine gamma values
+[guess1, guess2, guess3] = generateInitialGuesses(mu);
+[g1, dc1] = simpleNewtonRaphson(@(x) fdf1(mu, ax, x), guess1, options);
+[g2, dc2] = simpleNewtonRaphson(@(x) fdf2(mu, ax, x), guess2, options);
+[g3, dc3] = simpleNewtonRaphson(@(x) fdf3(mu, ax, x), guess3, options);
 
-    [x1, x2, x3] = g2x(mu, g1, g2, g3);
-    q = [x1, 0, 0, 0, 0, 0;
-         x2, 0, 0, 0, 0, 0;
-         x3, 0, 0, 0, 0, 0];
+[x1, x2, x3] = g2x(mu, g1, g2, g3);
+q = [x1, 0, 0, 0, 0, 0;
+    x2, 0, 0, 0, 0, 0;
+    x3, 0, 0, 0, 0, 0];
+
+if nargout > 1
+    didConverge = [dc1; dc2; dc3];
+end
+
 end
 
 function [g1, g2, g3] = x2g(mu, x1, x2, x3)
 %X2G convert the x coordinates to their respective gamma values
-    g1 = 1 - mu - x1;
-    g2 = x2 - 1 + mu;
-    g3 = -mu - x3;
+g1 = 1 - mu - x1;
+g2 = x2 - 1 + mu;
+g3 = -mu - x3;
 end
 
 function [x1, x2, x3] = g2x(mu, g1, g2, g3)
 %G2X convert gamma coordinates to their respective x values
-    x1 = 1 - mu - g1;
-    x2 = 1 - mu + g2;
-    x3 = -mu - g3;
+x1 = 1 - mu - g1;
+x2 = 1 - mu + g2;
+x3 = -mu - g3;
 end
 
 function [g1, g2, g3] = generateInitialGuesses(mu)
@@ -62,37 +78,37 @@ function [g1, g2, g3] = generateInitialGuesses(mu)
 %
 %   For this initial guess, I am just using the location of the Lagrange points, i.e., the
 %   location of the equilibrium solutions with no low-thrust acceleration present.
-    lagrangePoints = mtd.crtbp.computeLagrangePoints(mu);
-    [g1, g2, g3] = x2g(mu, lagrangePoints(1,1), lagrangePoints(2,1), lagrangePoints(3,1));
+lagrangePoints = mtd.crtbp.computeLagrangePoints(mu);
+[g1, g2, g3] = x2g(mu, lagrangePoints(1,1), lagrangePoints(2,1), lagrangePoints(3,1));
 end
 
 function [f, df] = fdf1(mu, a, g)
 %FDF1 helper function to evaluate function and derivative for x1 equilibrium solution
-    f = 1 - mu - g - (1 - mu) / (1 - g)^2 + mu / g^2 + a;
-    df = -1 - 2 * (1 - mu) / (1 - g)^3 - 2 * mu / g^3;
+f = 1 - mu - g - (1 - mu) / (1 - g)^2 + mu / g^2 + a;
+df = -1 - 2 * (1 - mu) / (1 - g)^3 - 2 * mu / g^3;
 end
 
 function [f, df] = fdf2(mu, a, g)
 %FDF2 helper function to evaluate function and derivative for x1 equilibrium solution
-    f = (1 - mu) / (1 + g)^2 + mu / g^2 - 1 + mu - g - a;
-    df = -2 * (1 - mu) / (1 + g)^3 - 2 * mu / g^3 - 1;
+f = (1 - mu) / (1 + g)^2 + mu / g^2 - 1 + mu - g - a;
+df = -2 * (1 - mu) / (1 + g)^3 - 2 * mu / g^3 - 1;
 end
 
 function [f, df] = fdf3(mu, a, g)
 %FDF3 helper function to evaluate function and derivative for x1 equilibrium solution
-    f = -mu - g + (1 - mu) / g^2 + mu / (g + 1)^2 + a;
-    df = -1 - 2*(1 - mu) / g^3 - 2 * mu / (g + 1)^3;
+f = -mu - g + (1 - mu) / g^2 + mu / (g + 1)^2 + a;
+df = -1 - 2*(1 - mu) / g^3 - 2 * mu / (g + 1)^3;
 end
 
 function [x, didConverge, nIter] = simpleNewtonRaphson(f, x0, options)
 %SIMPLENEWTONRAPHSON simple newton raphson function to solve for root
-    x = x0;
+x = x0;
+[g, dg] = f(x);
+nIter = 0;
+while abs(g) > options.tol && nIter < options.maxiter
+    x = x - g / dg;
     [g, dg] = f(x);
-    nIter = 0;
-    while abs(g) > options.tol && nIter < options.maxiter
-        x = x - g / dg;
-        [g, dg] = f(x);
-        nIter = nIter + 1;
-    end
-    didConverge = abs(g) <= options.tol;
+    nIter = nIter + 1;
+end
+didConverge = abs(g) <= options.tol;
 end
--- +mtd/+crtbp/computeJacobiConstant.m
+++ +mtd/+crtbp/computeJacobiConstant.m
@@ -1,11 +1,51 @@
-function jc = computeJacobiConstant(massRatio, qs)
+function [jc, varargout] = computeJacobiConstant(massRatio, qs)
 %COMPUTEJACOBICONSTANT compute the CRTBP Jacobi constant that is a scalar multiple of
-%the Hamiltonian
+%the Hamiltonian; optionally compute jacobian and hessian of function as well
+%
+%   The Jacobi constant is given by the function:
+%       JC = 2OMEGA(R) - V^2
+%   where OMEGA(R) is the pseduopotential that is only a function of the position vector R
+%   and V is the velocity magnitude.
 arguments
     massRatio (1, 1) double {mtd.crtbp.mustBeAValidMassRatio}
     qs (:, 6) double
 end
-omega = mtd.crtbp.computePseudopotential(massRatio, qs);
+
+% Determine number of states passed in for allocation purposes
+nStates = size(qs, 1);
+
+% If we are to compute the gradient of the Jacobi constant, we'll need the gradient of
+% omega and if we want to calculate the hessian, we'll need the hessian of omeg
+if nargout < 2
+    omega = mtd.crtbp.computePseudopotential(massRatio, qs);
+elseif nargout == 2
+    [omega, domega] = mtd.crtbp.computePseudopotential(massRatio, qs);
+elseif nargout ==3
+    [omega, domega, ddomega] =  mtd.crtbp.computePseudopotential(massRatio, qs);
+end
+
 v2 = sum(qs(:, 4:6).^2, 2);
 jc = 2 .* omega - v2;
+
+% If a second output is requested, we will provide the gradient of the Jacobi constant
+% with respect to the state variables
+if nargout > 1
+    djc = zeros(nStates, 6);
+    djc(:, 1:3) = 2 * domega;
+    djc(:, 4:6) = -2 * qs(:, 4:6);
+    varargout{1} = djc;
+end
+
+% Compute the hessian, actually relatively easy
+if nargout > 2
+    ddjc = zeros(6, 6, nStates);
+    ddjc(1:3, 1:3, :) = 2 .* ddomega;
+
+    % These three lines because `eye` doesn't work for 3D...
+    ddjc(4, 4, :) = -2;
+    ddjc(5, 5, :) = -2;
+    ddjc(6, 6, :) = -2;
+
+    varargout{2} = ddjc;
+end
 end
