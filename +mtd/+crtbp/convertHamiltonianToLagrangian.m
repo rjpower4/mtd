460f770af5c6396d5d67b2bff550ff00cde7316c
Rolfe Power <rolfepower4@gmail.com>
2021-12-29 01:35:21 -0500

    Fill out the hamiltonian -> lagrangian formulation

--- +mtd/+crtbp/convertHamiltonianToLagrangian.m
+++ +mtd/+crtbp/convertHamiltonianToLagrangian.m
@@ -1,4 +1,18 @@
 function qL = convertHamiltonianToLagrangian(qH)
 %CONVERTHAMILTONIANTOLAGRANGIAN convert states in the Hamiltonian basis to states in the
 %Lagrangian basis.
+%
+%   [QL] = CONVERTHAMILTONIANTOLAGRANGIAN(QH) convert the states in the hamiltonian basis
+%   that are the rows of QH to the lagrangian basis as the rows of QL.
+arguments
+    qH (:, 6) double % Input states in hamiltonian basis
+end
+
+% Conversion tensor
+W = mtd.util.angularVelocityTensor([0, 0, -1]);
+T = [eye(3), zeros(3); W, eye(3)];
+
+% Perform transformation as matrix multiplication
+qL = transpose(T * transpose(qH));
+
 end
